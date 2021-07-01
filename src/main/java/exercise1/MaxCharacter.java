1ef46f4b6e3d47591f2899215cadf52c2879a234
Michelle Zou <michelle_zou@hcl.com>
2021-07-01 14:58:42 -0400

    added exercise1

---
+++ src/main/java/exercise1/MaxCharacter.java
@@ -0,0 +1,39 @@
+package exercise1;
+
+import java.util.Scanner;
+
+public class MaxCharacter {
+
+	public static int maxCharacter(String s) {
+		int maxIndex = 0;
+		int count = 1;
+		int maxCount = -1;
+		char maxCh = s.charAt(0);
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = 1; j < s.length(); j++) {
+				if (s.charAt(i) == s.charAt(j)) {
+					count++;
+				} else {
+					if (count > maxCount) {
+						maxCount = count;
+						maxCh = s.charAt(i);
+					}
+					count = 1;
+				}
+			}
+		}
+
+		maxIndex = s.indexOf(maxCh);
+		return maxIndex;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Input String to find max character:");
+		String s = sc.nextLine();
+		System.out.print("The index of max character is: " + maxCharacter(s));
+		sc.close();
+	}
+
+}
